5510054b652ef8d163ffa6650e9835395e261321
Thom Mott <thommott@proton.me>
2025-12-31 02:28:19 -0800

    reorg & init new pages

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Dev environment for website development";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      pre-commit-hooks,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            excludes = [
+              ".*/submodules/.*"
+              "^submodules/.*"
+            ];
+            hooks = {
+              flake-checker.enable = true;
+              nixfmt-rfc-style = {
+                enable = true;
+                settings.width = 100;
+              };
+              statix = {
+                enable = true;
+                settings.ignore = [ "flake.lock" ];
+              };
+              deadnix.enable = true;
+              nil.enable = true;
+              shellcheck.enable = true;
+              shfmt.enable = true;
+              typos.enable = true;
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            git
+            jekyll
+          ];
+
+          shellHook = ''
+            		echo "Dev Env loaded."
+            		${self.checks.${system}.pre-commit-check.shellHook}
+          '';
+        };
+      }
+    );
+}
